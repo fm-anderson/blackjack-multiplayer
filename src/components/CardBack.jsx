dec5e5e6b93de97845f86298bd781b858bad2228
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2024-05-02 15:40:42 -0400

    feature: card back image

---
+++ src/components/CardBack.jsx
@@ -0,0 +1,13 @@
+function CardBack() {
+  return (
+    <li>
+      <img
+        src="https://deckofcardsapi.com/static/img/back.png"
+        alt="Card back"
+        className="w-24"
+      />
+    </li>
+  );
+}
+
+export default CardBack;
--- src/components/PlayerCards.jsx
+++ src/components/PlayerCards.jsx
@@ -1,25 +1,38 @@
+import CardBack from "./CardBack";
+
 function PlayerCards({ title, cards, onDraw }) {
   return (
     <div className="bg-green-200 p-6">
-      <h3 className="text-lg font-semibold">{title}</h3>
-      <p>Cards:</p>
+      <h3 className="mb-4 text-center text-lg font-semibold">{title}</h3>
       <ul className="flex gap-1">
-        {cards.map((card) => (
-          <li key={card.code}>
-            <img
-              src={card.image}
-              alt={`${card.value} of ${card.suit}`}
-              className="w-24"
-            />
-          </li>
-        ))}
+        {cards.length === 0 ? (
+          <>
+            <CardBack />
+            <CardBack />
+          </>
+        ) : (
+          cards.map((card) => (
+            <li key={card.code}>
+              <img
+                src={card.image}
+                alt={`${card.value} of ${card.suit}`}
+                className="w-24"
+              />
+            </li>
+          ))
+        )}
       </ul>
-      <button
-        className="mt-2 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-        onClick={onDraw}
-      >
-        Draw
-      </button>
+      <div className="mt-4 flex justify-between">
+        <button
+          className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+          onClick={onDraw}
+        >
+          Draw
+        </button>
+        <p className="rounded bg-slate-500 px-4 py-2 font-bold text-white">
+          Points: 0
+        </p>
+      </div>
     </div>
   );
 }
