ef8cd60c324b2bc3d17122b4d57a98c4a87f6eae
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2024-05-01 22:08:22 -0400

    feature: basic suffle and draw logic

---
+++ src/utils/api.js
@@ -0,0 +1,77 @@
+import { saveToLocalStorage } from "./helper";
+
+const baseUrl = import.meta.env.VITE_APP_BASE_URL;
+
+export const shuffleNewDeck = async (deckCount = 1) => {
+  const response = await fetch(
+    `${baseUrl}/new/shuffle/?deck_count=${deckCount}`,
+  );
+  const data = await response.json();
+  saveToLocalStorage("deckId", data.deck_id);
+  return data;
+};
+
+export const drawCards = async (deckId, count = 1) => {
+  const response = await fetch(`${baseUrl}/${deckId}/draw/?count=${count}`);
+  const data = await response.json();
+  return data;
+};
+
+export const reshuffleDeck = async (deckId, remaining = false) => {
+  const response = await fetch(
+    `${baseUrl}/${deckId}/shuffle/${remaining ? "?remaining=true" : ""}`,
+  );
+  const data = await response.json();
+  return data;
+};
+
+const createNewDeck = async (jokersEnabled = false) => {
+  const response = await fetch(
+    `${baseUrl}/new/${jokersEnabled ? "?jokers_enabled=true" : ""}`,
+  );
+  const data = await response.json();
+  return data;
+};
+
+const shufflePartialDeck = async (cards) => {
+  const response = await fetch(`${baseUrl}/new/shuffle/?cards=${cards}`);
+  const data = await response.json();
+  return data;
+};
+
+const addToPile = async (deckId, pileName, cards) => {
+  const response = await fetch(
+    `${baseUrl}/${deckId}/pile/${pileName}/add/?cards=${cards}`,
+  );
+  const data = await response.json();
+  return data;
+};
+
+const shufflePile = async (deckId, pileName) => {
+  const response = await fetch(
+    `${baseUrl}/${deckId}/pile/${pileName}/shuffle/`,
+  );
+  const data = await response.json();
+  return data;
+};
+
+const listPileCards = async (deckId, pileName) => {
+  const response = await fetch(`${baseUrl}/${deckId}/pile/${pileName}/list/`);
+  const data = await response.json();
+  return data;
+};
+
+const drawFromPile = async (deckId, pileName, cards) => {
+  const url = cards
+    ? `${baseUrl}/${deckId}/pile/${pileName}/draw/?cards=${cards}`
+    : `${baseUrl}/${deckId}/pile/${pileName}/draw/?count=2`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
+
+const returnCardsToDeck = async (deckId, cards) => {
+  const response = await fetch(`${baseUrl}/${deckId}/return/?cards=${cards}`);
+  const data = await response.json();
+  return data;
+};
