acae813a9e7b84c3fa9e344c836c4240a8e5e84c
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2024-05-10 23:34:02 -0400

    fix: adjust logic to usedeckofcards package

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,53 @@
-import { useState } from "react";
-import useDeckOfCards from "./hooks/useDeckOfCards";
+import useDeckOfCards from "usedeckofcards";
 import PlayerCards from "./components/PlayerCards";
+import { useState } from "react";
 
 function App() {
+  const [standA, setStandA] = useState(false);
+  const [standB, setStandB] = useState(false);
   const {
     deckId,
     cardsRemaining,
     isLoading,
-    playerACards,
-    playerBCards,
-    handleDrawPlayer,
     resetGame,
-    setPlayerACards,
-    setPlayerBCards,
+    pileCards,
+    drawAndAddToPile,
   } = useDeckOfCards();
+
+  const handleResetGame = () => {
+    setStandA(false);
+    setStandB(false);
+    resetGame();
+  };
 
   return (
     <div className="p-4">
       <h1>Blackjack Multiplayer</h1>
       <p>Deck ID: {deckId}</p>
-      <p>Cards Remaining: {cardsRemaining}</p>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <div className="mt-10 flex flex-row gap-5">
-          <PlayerCards
-            title="Player 1"
-            cards={playerACards}
-            onDraw={() => handleDrawPlayer(setPlayerACards, "pileA")}
-          />
-          <PlayerCards
-            title="Player 2"
-            cards={playerBCards}
-            onDraw={() => handleDrawPlayer(setPlayerBCards, "pileB")}
-          />
-        </div>
-      )}
+      <span className="flex gap-5">
+        <p>Cards Remaining: {cardsRemaining}</p>
+        {isLoading && <p>Loading...</p>}
+      </span>
+
+      <div className="mt-10 flex flex-row gap-5">
+        <PlayerCards
+          title="Helraham"
+          stand={standA}
+          setStand={setStandA}
+          pileCards={pileCards}
+          drawAndAddToPile={drawAndAddToPile}
+        />
+        <PlayerCards
+          title="Elstewart"
+          stand={standB}
+          setStand={setStandB}
+          pileCards={pileCards}
+          drawAndAddToPile={drawAndAddToPile}
+        />
+      </div>
       <button
         className="mt-2 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-        onClick={resetGame}
+        onClick={handleResetGame}
       >
         Reset
       </button>
--- src/components/PlayerCards.jsx
+++ src/components/PlayerCards.jsx
@@ -1,11 +1,18 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import CardBack from "./CardBack";
 import { calculatePoints } from "../utils/helper";
 
-function PlayerCards({ title, cards, onDraw }) {
+function PlayerCards({ title, pileCards, drawAndAddToPile, stand, setStand }) {
+  const cards = pileCards.filter((card) => card.pileName === title);
   const points = useMemo(() => calculatePoints(cards), [cards]);
-  const activeButton = "bg-blue-500 hover:bg-blue-700";
-  const disabledButton = "bg-slate-500 opacity-60";
+
+  const handleDeal = () => {
+    if (cards.length === 0) {
+      drawAndAddToPile(title, 2);
+    } else {
+      drawAndAddToPile(title);
+    }
+  };
 
   return (
     <div className="bg-green-200 p-6">
@@ -33,16 +40,16 @@ function PlayerCards({ title, cards, onDraw }) {
       </ul>
       <div className="mt-4 flex gap-2">
         <button
-          className={`rounded px-4 py-2 font-bold text-white ${points >= 21 ? disabledButton : activeButton}`}
-          onClick={onDraw}
-          disabled={points >= 21}
+          className={`rounded px-4 py-2 font-bold text-white ${points >= 21 || stand ? "bg-slate-500 opacity-60" : "bg-blue-500 hover:bg-blue-700"}`}
+          onClick={handleDeal}
+          disabled={points >= 21 || stand}
         >
           Deal
         </button>
         <button
-          className={`rounded px-4 py-2 font-bold text-white ${points >= 21 ? disabledButton : activeButton}`}
-          // onClick={TODO}
-          disabled={points >= 21}
+          className={`rounded px-4 py-2 font-bold text-white ${points >= 21 || stand ? "bg-slate-500 opacity-60" : "bg-blue-500 hover:bg-blue-700"}`}
+          onClick={() => setStand(true)}
+          disabled={points >= 21 || stand}
         >
           Stand
         </button>
